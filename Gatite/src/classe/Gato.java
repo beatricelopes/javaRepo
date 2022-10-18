5ff3e843a37c0c28d7e66ca17846d97cc60f1688
Beatrice <beatricelopes@outlook.com>
2022-10-17 22:10:27 -0300

    gato funcionando

---
+++ Gatite/src/classe/Gato.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classe;
+
+/**
+ *
+ * @author beatr
+ */
+public class Gato {
+    //atributos
+    private char sexo;
+    private float altura;
+    private float peso;
+    private String cor;
+    private String nome;
+    
+    public Gato(char sexo, float altura, float peso, String cor, String nome) {
+        this.sexo = sexo;
+        this.altura = altura;
+        this.peso = peso;
+        this.cor = cor;
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
